35ffc66fcc0c980a4c2fa81093f8d7276003b711
Tom Wood <tomwood1875@gmail.com>
2025-05-08 09:22:04 +0100

    chore: add clearer console logs

--- scripts.js
+++ scripts.js
@@ -3,22 +3,22 @@ let operator = null
 let secondNum = null
 
 function add(num1, num2) {
-    console.log(num1+num2)
+    console.log("add function:", num1+num2)
     return num1 + num2
 }
 
 function subtract (num1, num2) {
-    console.log(num1-num2)
+    console.log("subtract function:", num1-num2)
     return num1 - num2
 }
 
 function multiply (num1, num2) {
-    console.log(num1*num2)
+    console.log("multiply function:", num1*num2)
     return num1 * num2
 }
 
 function divide (num1, num2) {
-    console.log(num1/num2)
+    console.log("divide function:", num1/num2)
     return num1 / num2
 }
 
@@ -26,4 +26,3 @@ add(2,6)
 subtract(3,4)
 multiply(4,4)
 divide (10,2)
-
