d07c68679775485e4f3d479115aee46418191f3a
Tom Wood <tomwood1875@gmail.com>
2025-05-08 09:07:08 +0100

    feat: initialize calculator operation variables

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+let firstNum = null
+let operator = null
+let secondNum = null
+
 function add(num1, num2) {
     console.log(num1+num2)
     return num1 + num2
@@ -22,3 +26,4 @@ add(2,6)
 subtract(3,4)
 multiply(4,4)
 divide (10,2)
+
