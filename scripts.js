1dae580ae029cbe496702739f8c079e8fb0bb754
Tom Wood <tomwood1875@gmail.com>
2025-05-08 08:57:12 +0100

    feat: implement basic calculator operations

---
+++ scripts.js
@@ -0,0 +1,24 @@
+function add(num1, num2) {
+    console.log(num1+num2)
+    return num1 + num2
+}
+
+function subtract (num1, num2) {
+    console.log(num1-num2)
+    return num1 - num2
+}
+
+function multiply (num1, num2) {
+    console.log(num1*num2)
+    return num1 * num2
+}
+
+function divide (num1, num2) {
+    console.log(num1/num2)
+    return num1 / num2
+}
+
+add(2,6)
+subtract(3,4)
+multiply(4,4)
+divide (10,2)
